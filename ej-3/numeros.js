b2d0926d102ccfbc85577a11a5eaf7ae65a3af19
Sif Et tabaa <sifbusinesswork@gmail.com>
2022-10-25 13:34:34 +0200

    logic of even/true $ odd/false created

---
+++ ej-3/numeros.js
@@ -0,0 +1,11 @@
+let esPar = (numero) =>{
+    if (numero % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+} 
+
+// console.log(esPar(3)) // false
+// console.log(esPar(2)) //true
